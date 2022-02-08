a4a1a0c90dbff5d8ab0a4ba699ff8e73450a9cb0
muzhengyang <2592972482@qq.com>
2022-02-08 16:11:48 +0800

    add test file

---
+++ src/index.js
@@ -0,0 +1,10 @@
+/*
+ * @Autor: GeekMzy
+ * @Date: 2022-02-08 16:11:17
+ * @LastEditors: GeekMzy
+ * @LastEditTime: 2022-02-08 16:11:32
+ * @FilePath: \m-build\src\index.js
+ * @Author: desktop-1llkr2o
+ */
+
+console.log("this is a test cli project");
